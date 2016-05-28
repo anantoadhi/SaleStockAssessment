b5a711139b037585e6c324ce2f532c393304262e
anantoadhi <ananto.adhi@hotmail.com>
2016-05-28 22:41:25 +0700

    Assessment Purpose

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using SaleStockAssessment.Models;
+
+namespace SaleStockAssessment.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        static readonly IProductRepository repository = new ProductRepository();
+        private ProductRepository productRepository;
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return repository.GetAll();
+        }
+
+        public Product GetProduct(int id)
+        {
+            Product item = repository.Get(id);
+            if (item == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            return item;
+        }
+
+        public IEnumerable<Product> GetProductsByCategory(string category)
+        {
+            return repository.GetAll().Where(
+                p => string.Equals(p.Category, category, StringComparison.OrdinalIgnoreCase));
+        }
+
+        public Product PostProduct(Product item)
+        {
+            item = repository.Add(item);
+            return item;
+        }
+        
+        public void DeleteProduct(int id, Product product)
+        {
+            Product item = repository.Get(id);
+            if (item == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            repository.Remove(id);
+        }
+
+        public void PutProduct(int id, Product product)
+        {
+            product.Id = id;
+            if (repository.Update(product))
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+        }
+    } 
+}
---
+++ Models/ProductRepository.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SaleStockAssessment.Models
+{
+    public class ProductRepository : IProductRepository
+    {
+        private List<Product> products = new List<Product>();
+        private int _nextId = 1;
+        
+        public ProductRepository()
+        {
+            Add(new Product { Name = "Lee Cooper", catID=1, Category = "Pants", Price = 150000 });
+            Add(new Product { Name = "Uniqlo", catID=2, Category = "Clothes", Price = 250000 });
+            Add(new Product { Name = "Cardinal", catID = 1, Category = "Pants", Price = 175000 });
+            Add(new Product { Name = "Fossil", catID = 3, Category = "Watches", Price = 1750000 });           
+           
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            return products;
+        }
+
+        public Product Get(int id)
+        {
+            return products.Find(p => p.Id == id);
+        }
+
+        public Product Add(Product item)
+        {
+           
+            if (item == null)
+            {
+                throw new ArgumentNullException("item");
+            }
+            item.Id = _nextId++;
+            products.Add(item);
+            return item;
+        }
+
+        public void Remove(int id)
+        {
+            products.RemoveAll(p => p.Id == id);
+        }
+
+        public bool Update(Product item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException("item");
+            }
+            int index = products.FindIndex(p => p.Id == item.Id);
+
+            if (index == -1)
+            {
+                return false;
+            }
+
+            products.RemoveAt(index);
+            products.Add(item);
+            return true;
+        }
+    }
+}
